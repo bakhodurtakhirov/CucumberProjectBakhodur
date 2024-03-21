de9c1cfbcb982fc44cd49e206eb550615b0cd8ac
bakhodurtakhirov <bakhodur.takhirov@gmail.com>
2024-03-20 21:33:13 -0400

    For Jenkins Cucumber report

--- src/test/java/runnerClasses/_04_RunnerClassForGroups.java
+++ src/test/java/runnerClasses/_04_RunnerClassForGroups.java
@@ -6,7 +6,8 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/java/featurefiles",
         glue = "stepDefinitions",
-        tags = "@SmokeTest"
+        tags = "@SmokeTest",
+        plugin = {"pretty", "html:target/cucumber-pretty", "json:target/cucumber.json"} //For Jenkins Cucumber report
 )
 public class _04_RunnerClassForGroups extends AbstractTestNGCucumberTests {
 
